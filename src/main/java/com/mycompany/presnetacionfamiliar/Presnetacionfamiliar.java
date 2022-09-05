a798f5935065c287ffb569cc542e8a0134125517
[Daniel1358078] <[1358078@clases.edu.sv]>
2022-09-05 13:43:14 -0600

    variablesfamilia

---
+++ src/main/java/com/mycompany/presnetacionfamiliar/Presnetacionfamiliar.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.presnetacionfamiliar;
+
+/**
+ *
+ * @author daniel
+ */
+public class Presnetacionfamiliar {
+
+    public static void main(String[] args) {
+        int familia = 4;
+        System.out.println("mi madre es: ruth sanchez");
+        System.out.println("su edad es: 34");
+        System.out.println("mi padre es: francisco reyes");
+        System.out.println("su edad es: 37");
+        System.out.println("mi hermana es: tatiana sanchez");
+        System.out.println("su edad es: 10");
+        System.out.println("mi hermano es: daniel sanchez");
+        System.out.println("su edad es: 15");
+    }
+}
